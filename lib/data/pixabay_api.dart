147e4fdabc4d7a3629b37b93c5b39c632e583e27
libramin <libramin9@gmail.com>
2022-12-03 18:50:15 +0900

    usedMockitoTest_pixabayApi

--- lib/data/pixabay_api.dart
+++ lib/data/pixabay_api.dart
@@ -7,12 +7,14 @@ import '../model/photo.dart';
 import 'package:http/http.dart' as http;
 
 class PixabayApi implements PhotoApiRepo{
-  final baseUrl = 'https://pixabay.com/api/';
-  final apiKey = '31741555-b0ed9aca0f7a126912958be4c';
+  static const baseUrl = 'https://pixabay.com/api/';
+  static const apiKey = '31741555-b0ed9aca0f7a126912958be4c';
 
   @override
-  Future<List<Photo>> fetch(String query) async {
-    final response = await http.get(Uri.parse(
+  Future<List<Photo>> fetch(String query,{http.Client? client}) async {
+    client ??= http.Client();
+
+    final response = await client.get(Uri.parse(
         '$baseUrl?key=$apiKey&q=$query&image_type=photo'));
     Map<String, dynamic> jsonResponse = jsonDecode(response.body);
     Iterable hits = jsonResponse['hits']; //iterable 반복자
